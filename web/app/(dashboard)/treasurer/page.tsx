e2d03b54ccc13e2706e911f31e8b7a30d749b185
sajithktn <sajithsak3610@gmail.com>
2026-08-22 10:04:03 +0530

    feat: setup treasurer dashboard structure

--- web/app/(dashboard)/treasurer/page.tsx
+++ web/app/(dashboard)/treasurer/page.tsx
@@ -1,4 +1,4 @@
-import TreasurerDashboard from "@/features/dashboard/treasurer/components/TreasurerDashboard";
+import TreasurerDashboard from "../../../features/dashboard/treasurer/components/TreasurerDashboard";
 export default function TreasurerPage() {
   return <TreasurerDashboard />;
 }
